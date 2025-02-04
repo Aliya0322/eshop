9c35209f09d3c91f2bb09a9bc7af8e29f3860bbb
Алия Дюсембекова <DusembekovaAZ@gmail.com>
2025-02-04 16:46:35 +0300

    feat: init project

--- server/urls.py
+++ server/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path
+from shop.views import main_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_page),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from itertools import product
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from shop.models import Product
+
+def main_page(request: HttpRequest):
+    print(f"{request.method=}")
+    print(f"{request.content_type=}")
+    print(f"{request.GET}")
+    product_titles = Product.objects.values_list('title', flat=True)
+    titles = b''
+    for title in product_titles:
+        titles +=title.encode('utf-8')+b"\n"
+    print(f"{titles.decode("utf-8")=}")
+    return HttpResponse(titles)
