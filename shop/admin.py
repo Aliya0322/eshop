5ebea5af39c4d406a3a09d9437bb449fd1636317
Алия Дюсембекова <DusembekovaAZ@gmail.com>
2025-02-04 12:19:15 +0300

    feat: init project

--- shop/admin.py
+++ shop/admin.py
@@ -1,15 +1,29 @@
 from django.contrib import admin
+from django.contrib import messages
 
-from shop.models import (ProductImage, Product, Attribute)
+from shop.models import ProductImage, Product, Attribute
+from shop.filters import ProductStockFilter
 
 
 # admin.site.register(Product)
 # admin.site.register(ProductImage)
 # admin.site.register(Attribute)
+@admin.action(description='Обнулить остатки')
+def set_zero_stock(modeladmin, request, queryset):
+    queryset.update(stock=0)
+    modeladmin.message_user(
+        request,
+        "Остатки товаров обнулены.",
+        level=messages.SUCCESS
+    )
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+    list_filter = ('attributes', ProductStockFilter,)
     list_display = ('title', 'price', 'stock')
+    actions = (set_zero_stock,)
 
 
 @admin.register(ProductImage)
@@ -18,5 +32,5 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 @admin.register(Attribute)
-class ProductAdmin(admin.ModelAdmin):
+class AttributeAdmin(admin.ModelAdmin):
     list_display = ('name',)
