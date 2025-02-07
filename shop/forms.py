951b441aacf19c9128a6e958d915284ff1cb0852
Алия Дюсембекова <DusembekovaAZ@gmail.com>
2025-02-07 20:50:00 +0300

    init commit

--- shop/forms.py
+++ shop/forms.py
@@ -6,7 +6,6 @@ import re
 class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        print(f"Fields in the form: {list(self.fields.keys())}")  # Вывод списка полей в консоль
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
             field.help_text = None
